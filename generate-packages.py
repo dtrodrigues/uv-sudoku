19d283b35f50fe1bb1573065a4333a01f8fb9953
Dustin Rodrigues <dust.rod@gmail.com>
2025-07-07 14:10:36 -0400

    clean up code

--- generate-packages.py
+++ generate-packages.py
@@ -11,20 +11,20 @@ shutil.rmtree('wheels', ignore_errors=True)
 os.makedirs('wheels', exist_ok=True)
 
 def generate_package(row, col, digit):
-            deplist = set()
-            for x in range(0,9):
-                if x != row:
-                    deplist.add(f"\"{BASE_NAME}_{x+1}{col+1}!={digit}\"")
-            for x in range(0,9):
-                if x != col:
-                    deplist.add(f"\"{BASE_NAME}_{row+1}{x+1}!={digit}\"")
-            myPos = row * 9 + col
-            box = intToBox[myPos]
-            for x in boxes[box]:
-                if x != myPos:
-                    deplist.add(f"\"{BASE_NAME}_{x//9+1}{x%9+1}!={digit}\"")
+    deplist = set()
+    for x in range(0,9):
+        if x != row:
+            deplist.add(f"\"{BASE_NAME}_{x+1}{col+1}!={digit}\"")
+    for x in range(0,9):
+        if x != col:
+            deplist.add(f"\"{BASE_NAME}_{row+1}{x+1}!={digit}\"")
+    myPos = row * 9 + col
+    box = intToBox[myPos]
+    for x in boxes[box]:
+        if x != myPos:
+            deplist.add(f"\"{BASE_NAME}_{x//9+1}{x%9+1}!={digit}\"")
 
-            tomlval = f"""
+    tomlval = f"""
 [project]
 name = "{BASE_NAME}_{row+1}{col+1}"
 version = "{digit}"
@@ -35,15 +35,16 @@ dependencies = [{", ".join(deplist)}]
 requires = ["uv_build>=0.7.19,<0.8"]
 build-backend = "uv_build"
 """
-            os.makedirs(f"packages/{BASE_NAME}_{row+1}{col+1}_{digit}", exist_ok=True)
-            with open(f"packages/{BASE_NAME}_{row+1}{col+1}_{digit}/pyproject.toml", "w") as f:
-                f.write(tomlval)
+    currentPackageDir = f"packages/{BASE_NAME}_{row+1}{col+1}_{digit}"
+    os.makedirs(currentPackageDir, exist_ok=True)
+    with open(f"{currentPackageDir}/pyproject.toml", "w") as f:
+        f.write(tomlval)
 
-            os.makedirs(f"packages/{BASE_NAME}_{row+1}{col+1}_{digit}/src/{BASE_NAME}_{row+1}{col+1}", exist_ok=True)
-            with open(f"packages/{BASE_NAME}_{row+1}{col+1}_{digit}/src/{BASE_NAME}_{row+1}{col+1}/__init__.py", "w") as f:
-                pass
+    os.makedirs(f"{currentPackageDir}/src/{BASE_NAME}_{row+1}{col+1}", exist_ok=True)
+    with open(f"{currentPackageDir}/src/{BASE_NAME}_{row+1}{col+1}/__init__.py", "w") as f:
+        pass
 
-            subprocess.run(["uv", "build", "--wheel", "-o", "../../wheels/"], cwd=f"packages/{BASE_NAME}_{row+1}{col+1}_{digit}", capture_output=True, text=True)
+    subprocess.run(["uv", "build", "--wheel", "-o", "../../wheels/"], cwd=currentPackageDir, capture_output=True, text=True)
 
 
 
